fa069f41a35999195f5a681621ea38133f08009f
Илья Кобрунов <elijah.kobrunov@gmail.com>
2023-11-03 20:01:47 +0300

    v 1.1.0
    - Начата переработка архитектуры
    - Доравботка логики приложения

--- CommitModel.cs
+++ CommitModel.cs
@@ -8,15 +8,36 @@ namespace CommitContentCreater
 {
     public class CommitModel
     {
-        public DateTime Date { get; set; }
+        private DateTime _Date { get; set; }
 
         public string? StringDate { get; set; } = null;
 
-        public Version? Version { get; set; } = null;
+        public DateTime Date
+        {
+            get => _Date;
+            set
+            {
+                StringDate = null;
+                _Date = value;
+            }
+        }
 
-        public string Auther { get; set; } = null;
+        public Version Version { get; set; } = null;
 
-        public List<string> Lines { get; } = new List<string>();
+        public string Auther { get; set; } = "";
 
+        private List<CommitLineModel> _Lines { get; } = new List<CommitLineModel>();
+        
+        public CommitLineModel[] Lines => _Lines.ToArray();
+
+        public void AppendLine(string line)
+        {
+
+        }
+
+        public void AppendLine(CommitLineModel line)
+        {
+
+        }
     }
 }
